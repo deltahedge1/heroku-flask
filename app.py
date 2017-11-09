6a6fba47646c0f82017161ed90080d719fdcca6c
Hassan <ihassan1@kpmg.com>
2017-11-09 23:52:59 +1100

    upload test

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request
 from nltk import sent_tokenize, word_tokenize
 import docx2txt
+import os
 
 app = Flask(__name__)
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 @app.route("/")
 def index():
@@ -11,17 +14,23 @@ def index():
 @app.route("/upload", methods=["GET","POST"])
 def upload():
 
-    r = []
-    
+    target = os.path.join(APP_ROOT, "static/")
+
+    if not os.path.isdir(target):
+        os.mkdir(target)
+        
     for file in request.files.getlist("file"):
         filename = file.filename
-        #destination = "/".join([target, filename])  
-    
-    f = open(filename, "r")
+        destination = "/".join([target, filename])
+        file.save(destination)
+
+
+    f = open(destination, "r")
     r = [word for sentence in f.readlines() for word in word_tokenize(sentence)]
-    
     f.close()
-        
+
+    os.remove(destination)
+    
     return render_template("completed.html", r = r)
 
 if __name__ == '__main__':
