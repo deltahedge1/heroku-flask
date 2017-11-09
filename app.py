0759492eb2bf1a6f285ff75de954dfdf452cc9e3
Hassan <ihassan1@kpmg.com>
2017-11-09 22:55:17 +1100

    trying with opening a text file

--- app.py
+++ app.py
@@ -2,16 +2,18 @@ from flask import Flask, render_template
 from textblob import TextBlob
 from nltk import word_tokenize
 
-a = TextBlob("Ishtiaq is my name")
-b = list(a.words)
+f = open("test1.txt","r")
 
-c = word_tokenize("Samantha is nice")
+sentences = f.readlines()
+
+list1 = [word for sentence in sentences for word in word_tokenize(sentence)]
+
                   
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-           return render_template("index.html", r = c)
+           return render_template("index.html", r = list1)
 
 if __name__ == "__main__":
            app.run()
