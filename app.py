b078b8f4b43998221985f3197c5c7dc1f626a9e8
Hassan <ihassan1@kpmg.com>
2017-11-09 17:26:53 +1100

    second

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from textblob import TextBlob
 
-a = TextBlob("This is a sentence")
+a = TextBlob("Ishtiaq is my name")
 b = list(a.words)
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-           return "hello, world"
+           return "hello" + str(b[0])
 
 if __name__ == "__main__":
            app.run()
